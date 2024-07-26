03a7e3b1cd1360df7c303c26af81825f20e96eab
timur4ik1996 <timurmahianov659@gmail.com>
2024-07-26 13:21:43 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+first = 32
+second = 43
+third = 32
+if first == second and second == third and first == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
